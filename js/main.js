b11fc63ab96968e2990378da1ec5f6e5a51a69b5
Alex Bepple <alex@bepple.de>
2015-02-14 19:32:05 +0100

    programmatically decorate missing properties

--- js/main.js
+++ js/main.js
@@ -41,18 +41,28 @@ var validate = validator(schema);
 validate(json);
 
 var r = require('ramda');
-var isWrongType = r.propEq('message', 'is the wrong type');
-var fieldsWithWrongType = r.pluck('field')(r.filter(isWrongType, validate.errors));
 
-var fieldToPathComponents = r.pipe(r.split('.'), r.tail, r.append('value'));
+var errorToPathComponents = r.pipe(r.prop('field'), r.split('.'), r.tail);
 var prependString = r.useWith(r.replace(/^/), r.identity);
 var pathComponentsToSelector = r.pipe(r.map(prependString('.')), r.join(' '));
-var fieldToSelector = r.pipe(fieldToPathComponents, pathComponentsToSelector);
 
-var selectorsForWrongType = r.map(fieldToSelector, fieldsWithWrongType);
-console.log(selectorsForWrongType);
+var isWrongType = r.propEq('message', 'is the wrong type');
+var selectorsWrongType = r.pipe(
+    r.filter(isWrongType),
+    r.map(errorToPathComponents),
+    r.map(r.append('value')),
+    r.map(pathComponentsToSelector)
+)(validate.errors);
 
-$(r.join(',', selectorsForWrongType)).addClass('validation-wrong-type');
-$('.b .bb').addClass('validation-missing');
+var isMissing = r.propEq('message', 'is required');
+var selectorsMissing = r.pipe(
+    r.filter(isMissing),
+    r.map(errorToPathComponents),
+    r.map(pathComponentsToSelector)
+)(validate.errors);
+
+
+$(r.join(',', selectorsWrongType)).addClass('validation-wrong-type');
+$(r.join(',', selectorsMissing)).addClass('validation-missing');
 $('.b .bc').addClass('validation-additional');
 
